df6185edf954d62f371fdbb5cbdc13ee8c1d39ec
Armando Hernandez <armhzjz@pm.me>
2020-12-05 20:20:46 +0100

    change name of starter script (2)

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$1" = "--help" || "$1" = "-h" ]]; then
-    echo "Build the poky-enhanced image and install startCropsPokyCont.sh under"
+    echo "Build the poky-enhanced image and install \"poky_startContnr\" under"
     echo "/usr/local/bin."
     echo "If \"--no-install\" is passed as argument, only the image will be built"
     echo "without any script installation on the aforementioned directory."
@@ -16,9 +16,10 @@ else
 fi
 
 if [ ! "$1" = "--no-install" ]; then
-    cp startCropsPokyCont.sh /usr/local/bin
-    chmod +x startCropsPokyCont.sh
+    cp ./files/poky_startContnr /usr/local/bin
+    chmod +x poky_startContnr
     echo "Installation succeded."
+    echo "'poky_startContnr' can be called from within your top project's path."
 else
     echo "Docker image built. No installation of starting script was done."
 fi
